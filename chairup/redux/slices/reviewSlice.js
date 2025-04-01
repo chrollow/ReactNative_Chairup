e76af8cfc7abfa77726e8cb9e928a439b57e7bd2
chrollow <128967841+chrollow@users.noreply.github.com>
2025-04-01 16:28:14 +0800

    merrgeeee

--- chairup/redux/slices/reviewSlice.js
+++ chairup/redux/slices/reviewSlice.js
@@ -3,11 +3,7 @@ import { getProductReviews, getUserReviews } from '../../Context/Actions/Product
 import axios from 'axios';
 import * as SecureStore from 'expo-secure-store';
 
-<<<<<<< HEAD
 const BASE_URL = 'http://192.168.1.36:3000';
-=======
-const BASE_URL = 'http://192.168.100.11:3000';
->>>>>>> feature/fix-ui
 
 export const createReview = createAsyncThunk(
   'reviews/createReview',
